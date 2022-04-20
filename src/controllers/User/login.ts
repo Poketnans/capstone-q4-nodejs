eba3506f6171a7068c52a0c60421d051508c748b
Etnan Sousa <poketnans@gmail.com>
2022-04-20 18:33:46 -0300

    fix: comments uncompleted feature

--- src/controllers/User/login.ts
+++ src/controllers/User/login.ts
@@ -1,26 +1,26 @@
-import { Request, Response } from "express"
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
-import jwtConfig from "../../configs"
-import httpStatus from 'http-status'
-/* import userLogin from "../repositories/userLogin" */
+// import { Request, Response } from "express"
+// import jwt from "jsonwebtoken"
+// import bcrypt from "bcrypt"
+// import jwtConfig from "../../configs"
+// import httpStatus from 'http-status'
+// /* import userLogin from "../repositories/userLogin" */
 
-const loginController = async (req: Request, res: Response) => {
+// const loginController = async (req: Request, res: Response) => {
 
-    const { email, password } = req.body
-    const user = await userLogin(email)
+//     const { email, password } = req.body
+//     const user = await userLogin(email)
 
-    const match = await bcrypt.compare(password, user.password)
+//     const match = await bcrypt.compare(password, user.password)
 
-    if (!match) {
-        return res.status(401).json({ message: "Wrong email/password" })
-    }
+//     if (!match) {
+//         return res.status(401).json({ message: "Wrong email/password" })
+//     }
 
-    const token = jwt.sign({ user }, jwtConfig.secretKey, {
-        expiresIn: jwtConfig.expiresIn
-    })
+//     const token = jwt.sign({ user }, jwtConfig.secretKey, {
+//         expiresIn: jwtConfig.expiresIn
+//     })
 
-    return res.status(httpStatus.CREATED).json({ token })
-}
+//     return res.status(httpStatus.CREATED).json({ token })
+// }
 
-export default loginController
+// export default loginController
