2bb88cb05aff041268d885d5456120a843bba7c8
Etnan Sousa <poketnans@gmail.com>
2022-04-20 19:09:02 -0300

    refactor: ajusts required error message

--- src/schemas/userCreate.schema.ts
+++ src/schemas/userCreate.schema.ts
@@ -1,15 +1,27 @@
-import * as yup from "yup";
-import { v4 as uuidv4 } from "uuid";
-import bcrypt from "bcrypt";
+import * as yup from 'yup';
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
 
-const requiredText = "Campo obrigatório"
 const userSchema = yup.object().shape({
-  id : yup.string().nullable().default(()=> uuidv4()),
-  name: yup.string().required(`${requiredText} name.`).max(50, "maximum characters 50 ."),
-  email: yup.string().email("format invalid for email.").required(`${requiredText} email.`),
-  password: yup.string().required(`${requiredText} password.`).min(6,"minimum characters 6").transform((value)=> bcrypt.hashSync(value,10) ),
+  id: yup
+    .string()
+    .nullable()
+    .default(() => uuidv4()),
+  name: yup.string().required().max(50, 'maximum characters 50 .'),
+  email: yup.string().email('format invalid for email.').required(),
+  password: yup
+    .string()
+    .required()
+    .min(6, 'minimum characters 6')
+    .transform((value) => bcrypt.hashSync(value, 10)),
   employed: yup.boolean().default(false),
-  createdAt : yup.date().nullable().default(()=> new Date()),
-  updatedAt : yup.date().nullable().default(()=> new Date()),
-})
+  createdAt: yup
+    .date()
+    .nullable()
+    .default(() => new Date()),
+  updatedAt: yup
+    .date()
+    .nullable()
+    .default(() => new Date()),
+});
 export default userSchema;
