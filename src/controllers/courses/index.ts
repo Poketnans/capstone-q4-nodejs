fb30c86627bb47f8dc036b8e9c44089e70b98e0f
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-04-24 15:20:42 -0300

    feat: finish get one course

---
+++ src/controllers/courses/index.ts
@@ -0,0 +1,6 @@
+import CourseGetOneControler from "./getOne.controllers";
+
+export {
+  // eslint-disable-next-line import/prefer-default-export
+  CourseGetOneControler
+}
---
+++ src/services/courses/getOne.services.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { EntityNotFoundError } from "typeorm";
+import { ErrorHandler } from "../../errors";
+import { CourseRepository } from "../../repositories";
+
+const CourseGetOneServices =async (req: Request) => {
+  try {
+    
+    const { uuid } = req.params;
+    const findCourse = await new CourseRepository().findOneOrFail({"id": uuid});
+    return findCourse;
+
+  } catch (e) {
+
+    if(e instanceof EntityNotFoundError){
+      throw new ErrorHandler(404, `course not found.`);
+
+    }
+    throw new ErrorHandler(400, `${e.message}`);
+        
+  }
+}
+export default CourseGetOneServices;
