49448cbc9c102b12b23f493d862604965aebc624
Etnan Sousa <poketnans@gmail.com>
2022-04-25 08:48:16 -0300

    feat: implements initial ProjectRepository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,6 +1,11 @@
 import UserRepository from './user.repository';
 import CategoryRepository from './category/category.repository';
 import CourseRepository from './course/course.repository';
+import ProjectRepository from './project/project.repository';
 
-export { UserRepository, CourseRepository, CategoryRepository };
-
+export {
+  UserRepository,
+  CourseRepository,
+  CategoryRepository,
+  ProjectRepository,
+};
