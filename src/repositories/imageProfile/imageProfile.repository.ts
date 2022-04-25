b6a16fd25a5b4cd23fa994944c9a7c33dc110793
Pedro <pedromenimen@gmail.com>
2022-04-25 11:34:42 -0300

    feat: basic image functions created

--- src/repositories/imageProfile/imageProfile.repository.ts
+++ src/repositories/imageProfile/imageProfile.repository.ts
@@ -1,6 +1,6 @@
 import { getRepository, Repository } from 'typeorm';
 import Image from '../../entities/Image';
-import { ImageProfile } from './interfaces';
+import { IImage, ImageProfile } from './interfaces';
 
 class ImageProfileRepository implements ImageProfile {
   private ormRepository: Repository<Image>;
@@ -8,6 +8,14 @@ class ImageProfileRepository implements ImageProfile {
   constructor() {
     this.ormRepository = getRepository(Image);
   }
+
+  saveImageProfile = (imageInfo: IImage) => this.ormRepository.save(imageInfo);
+
+  getOneImage = (imageId: string) =>
+    this.ormRepository.findOne({ id: imageId });
+
+  updateImage = (imageId: string, updatedImage: IImage) =>
+    this.ormRepository.update({ id: imageId }, updatedImage);
 }
 
 export default ImageProfileRepository;
