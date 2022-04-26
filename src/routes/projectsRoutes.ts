3fd7fe2a47731903675fcbcaec44ee342907f0f4
Pedro <pedromenimen@gmail.com>
2022-04-26 15:24:11 -0300

    fix: schema validator added to update project function

--- src/routes/projectsRoutes.ts
+++ src/routes/projectsRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import { updateProjectController } from "../controllers/projects"
+import { validateSchemaMiddleware } from "../middlewares"
+import updateProjectSchema from "../schemas/update.project.schema"
 
 const projectsRoutes = Router()
 
 projectsRoutes.get("")
 projectsRoutes.get("/:uuid")
 projectsRoutes.post("")
-projectsRoutes.patch("", updateProjectController)
+projectsRoutes.patch("/:uuid", validateSchemaMiddleware(updateProjectSchema), updateProjectController)
 projectsRoutes.delete("")
 
 export default projectsRoutes
