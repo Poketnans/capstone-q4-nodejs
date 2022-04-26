c20c427c9b35827c839fb0b7743db22718a6edde
Etnan Sousa <poketnans@gmail.com>
2022-04-26 10:48:45 -0300

    feat: implements CoursePeriodRepository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -5,6 +5,7 @@ import FollowerRepository from './follower/follower.repository';
 import ImageProfileRepository from './imageProfile/imageProfile.repository';
 import ProjectRepository from './project/project.repository';
 import CourseReviewRepository from './courseReview/courseReview.repository';
+import CoursePeriodRepository from './coursePeriod/coursePeriod.repository';
 
 export {
   UserRepository,
@@ -14,4 +15,5 @@ export {
   ImageProfileRepository,
   ProjectRepository,
   CourseReviewRepository,
+  CoursePeriodRepository,
 };
