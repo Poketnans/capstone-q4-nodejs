9ffc7746c00a3e53d1dc64294f467f06f978e799
Pedro <pedromenimen@gmail.com>
2022-04-26 10:38:47 -0300

    fix: user now  is created with image

--- src/services/users/userCreate.service.ts
+++ src/services/users/userCreate.service.ts
@@ -1,19 +1,47 @@
-import { QueryFailedError } from "typeorm";
-import { IUser } from "../../types/user";
-import { UserRepository } from "../../repositories";
-import { ErrorHandler } from "../../errors";
+import { QueryFailedError } from 'typeorm';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import { IUser } from '../../types/user';
+import { ImageProfileRepository, UserRepository } from '../../repositories';
+import { ErrorHandler } from '../../errors';
+import { IImage } from '../../repositories/imageProfile/interfaces';
 
-const createUserService = async (user: IUser) =>{
+dotenv.config();
+
+const createUserService = async (user: IUser) => {
   try {
-    const { password, ...newUser } = await new UserRepository().saveUser(user);
-    return newUser
+    const imageInfo: IImage = {
+      mimetype: 'mimetype/png',
+      name: 'default.png',
+      binary: JSON.stringify(
+        Buffer.from(
+          fs.readFileSync(
+            `${
+              __dirname.split(
+                process.env.NODE_ENV === 'production' ? 'dist' : 'src'
+              )[0]
+            }assets/default.png`,
+            'base64'
+          ),
+          'base64'
+        )
+      ),
+    };
+
+    const newImage = await new ImageProfileRepository().saveImageProfile(
+      imageInfo
+    );
+    // eslint-disable-next-line no-param-reassign
+    user.image = newImage;
+    const { password, image, ...newUser } = await new UserRepository().saveUser(
+      user
+    );
+    return newUser;
   } catch (e) {
-    
-    if ( e instanceof QueryFailedError){
+    if (e instanceof QueryFailedError) {
       throw new ErrorHandler(400, `${e.driverError.detail}`);
     }
     throw new ErrorHandler(400, `${e.message}`);
   }
-
-}
+};
 export default createUserService;
