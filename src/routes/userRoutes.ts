0a7972e43d73f3b2a1409b84b1993d04d7156969
Etnan Sousa <poketnans@gmail.com>
2022-04-28 12:32:51 -0300

    fix: solves conflicts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,10 +2,7 @@ import { Router } from 'express';
 import getUsersController from '../controllers/User/getAll';
 
 import { updateUserSchema, userSchema, loginSchema } from '../schemas';
-<<<<<<< HEAD
 import loginController from '../controllers/User/login';
-=======
->>>>>>> 0feea81a1429b8ebf2aa199787f6d975f0ee4b7e
 
 import {
   updateUserImageValidator,
@@ -26,11 +23,7 @@ const userRoutes = Router();
 userRoutes.get('/profile');
 userRoutes.patch('', validateSchemaMiddleware(updateUserSchema), updateUser);
 userRoutes.get('', getUsersController);
-<<<<<<< HEAD
 userRoutes.get('/:user_id', validateAuth, getOneController);
-=======
-
->>>>>>> dc82e011e42ba0c07426538d1e3d08b147cb9e28
 userRoutes.post(
   '/register',
   validateSchemaMiddleware(userSchema),
@@ -55,5 +48,3 @@ userRoutes.patch(
 );
 
 export default userRoutes;
-
-
