f337e88959604db1bae36d47418da8b3b60a6f2b
Etnan Sousa <poketnans@gmail.com>
2022-04-28 19:55:17 -0300

    refactor: removes unused routes

--- src/routes/reviewsRoutes.ts
+++ src/routes/reviewsRoutes.ts
@@ -1,22 +1,37 @@
-import { Router } from "express"
-import { updateReviewController, deleteReviewController } from "../controllers/Review";
-import { validateAuth, validateSchemaMiddleware, validatePermissionReviewMiddleware } from "../middlewares";
-import { userReviewUpdate, reviewSchema } from "../schemas";
+import { Router } from 'express';
+import {
+  updateReviewController,
+  deleteReviewController,
+} from '../controllers/Review';
+import {
+  validateAuth,
+  validateSchemaMiddleware,
+  validatePermissionReviewMiddleware,
+} from '../middlewares';
+import { userReviewUpdate, reviewSchema } from '../schemas';
 import createReviewController from '../controllers/Review/createReview';
 
-const reviewsRoutes = Router()
+const reviewsRoutes = Router();
 
-reviewsRoutes.get("")
-reviewsRoutes.get("/:uuid")
-reviewsRoutes.post("", validateAuth, validateSchemaMiddleware(reviewSchema), createReviewController)
-reviewsRoutes.patch("/:id", 
+// reviewsRoutes.get('');
+reviewsRoutes.post(
+  '',
+  validateAuth,
+  validateSchemaMiddleware(reviewSchema),
+  createReviewController
+);
+reviewsRoutes.patch(
+  '/:id',
   validateAuth,
   validatePermissionReviewMiddleware,
   validateSchemaMiddleware(userReviewUpdate),
-  updateReviewController)
-reviewsRoutes.delete("/:id", 
+  updateReviewController
+);
+reviewsRoutes.delete(
+  '/:id',
   validateAuth,
   validatePermissionReviewMiddleware,
-  deleteReviewController);
+  deleteReviewController
+);
 
-export default reviewsRoutes
+export default reviewsRoutes;
