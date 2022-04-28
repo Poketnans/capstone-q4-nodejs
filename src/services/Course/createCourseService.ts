4e9c62cb92bbd28776c855ac227e43fba0607f1f
Etnan Sousa <poketnans@gmail.com>
2022-04-28 17:15:28 -0300

    feat: refactor course create service for consider custom erros

--- src/services/Course/createCourseService.ts
+++ src/services/Course/createCourseService.ts
@@ -1,8 +1,5 @@
-import { QueryFailedError } from 'typeorm';
-import httpStatus from 'http-status';
 import Course from '../../entities/Course';
 import { ICourse } from '../../repositories/course/interfaces';
-import { ErrorHandler } from '../../errors';
 import CourseRepository from '../../repositories/course/course.repository';
 import {
   CategoryRepository,
@@ -12,37 +9,30 @@ import {
 } from '../../repositories';
 
 const createCourseService = async (id: string, reqValidated: ICourse) => {
-  try {
-    const courseValidated: ICourse = reqValidated;
+  const courseValidated: ICourse = reqValidated;
 
-    const courseOwner = await new UserRepository().getOneUser(id);
-    const category = await new CategoryRepository().getCategory(
-      reqValidated.id_category
-    );
-    const mode = await new CourseModeRepository().getOneMode(
-      reqValidated.id_mode
-    );
-    const period = await new CoursePeriodRepository().getOneMode(
-      reqValidated.id_period
-    );
+  const courseOwner = await new UserRepository().getOneUser(id);
+  const category = await new CategoryRepository().getCategory(
+    reqValidated.id_category
+  );
+  const mode = await new CourseModeRepository().getOneMode(
+    reqValidated.id_mode
+  );
+  const period = await new CoursePeriodRepository().getOnePeriod(
+    reqValidated.id_period
+  );
 
-    courseValidated.user_owner = courseOwner;
-    courseValidated.category = category;
-    courseValidated.mode = mode;
-    courseValidated.period = period;
+  courseValidated.user_owner = courseOwner;
+  courseValidated.category = category;
+  courseValidated.mode = mode;
+  courseValidated.period = period;
 
-    delete courseValidated.user_owner.password;
+  delete courseValidated.user_owner.password;
 
-    const courseCreated: Course = await new CourseRepository().saveCourse(
-      courseValidated
-    );
+  const courseCreated: Course = await new CourseRepository().saveCourse(
+    courseValidated
+  );
 
-    return courseCreated;
-  } catch (e: any) {
-    if (e instanceof QueryFailedError) {
-      throw new ErrorHandler(httpStatus.CONFLICT, `${e.driverError.detail}`);
-    }
-    throw new ErrorHandler(httpStatus.BAD_REQUEST, `${e.message}`);
-  }
+  return courseCreated;
 };
 export default createCourseService;
