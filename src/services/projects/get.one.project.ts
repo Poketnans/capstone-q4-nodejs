26f9eaf8320cad4f59f0162cda36e18b38399a8b
Etnan Sousa <poketnans@gmail.com>
2022-04-28 20:35:38 -0300

    refactor: ajusts get project response body

--- src/services/projects/get.one.project.ts
+++ src/services/projects/get.one.project.ts
@@ -4,7 +4,12 @@ import { ProjectRepository } from '../../repositories';
 
 const getOneProjectService = async (id: string) => {
   try {
-    const foundProject = await new ProjectRepository().getOne(id, ['user_owner']);
+    const foundProject = await new ProjectRepository().getOne(id, [
+      'user_owner',
+    ]);
+
+    delete foundProject.user_owner.password;
+
     return foundProject;
   } catch (e: any) {
     throw new ErrorHandler(httpStatus.NOT_FOUND, 'project not found');
