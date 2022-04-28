7bc99d9e5d9164dc6762d160c0e3f525388e8819
Etnan Sousa <poketnans@gmail.com>
2022-04-28 12:56:27 -0300

    refactor: rmoves console.log

--- src/errors/error.ts
+++ src/errors/error.ts
@@ -15,8 +15,6 @@ const handleError = (err: unknown, res: Response) => {
   if (err instanceof ErrorHandler) {
     const { status, message } = err;
 
-    console.log(status, message);
-
     return res.status(status).json({ error: message });
   }
 
