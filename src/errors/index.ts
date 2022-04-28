075ba9736b76a5afa163ae4e0cd9eb3b156de0f4
Etnan Sousa <poketnans@gmail.com>
2022-04-28 15:48:39 -0300

    feat: ajusts imports

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,5 +1,14 @@
-import { ErrorHandler, handleError } from "./error"
+import { ErrorHandler, handleError } from './error';
+import UuidMalformedError from './uuidMalformed.error';
+import CategoryNotFoundError from './categoryNotFound.error';
+import ModeNotFoundError from './modeNotFound.error';
+import PeriodNotFoundError from './periodNotFound.error';
 
 export {
-  ErrorHandler, handleError
+  ErrorHandler,
+  handleError,
+  UuidMalformedError,
+  CategoryNotFoundError,
+  ModeNotFoundError,
+  PeriodNotFoundError,
 };
