55bf73bbc6cc1eeb1bd7ceecc39aecf6dbc720a5
Pedro <pedromenimen@gmail.com>
2022-04-29 09:47:36 -0300

    fix: image id returned with image id

--- src/services/users/getOne.service.ts
+++ src/services/users/getOne.service.ts
@@ -1,8 +1,10 @@
 import { UserRepository } from '../../repositories';
 
 const getOneService = async (id: string) => {
-  const userFounded = await new UserRepository().getOneUser(id);
-
+  const userFounded = await new UserRepository().getOneUser(id, ["image"]);
+  delete userFounded.image.binary
+  delete userFounded.image.name
+  delete userFounded.image.mimetype
   if (!userFounded) {
     throw new Error('User not found');
   }
